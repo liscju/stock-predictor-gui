0b9cc3568c9f63e2385d0a193fc2a39a75b94d0c
liscju <piotr.listkiewicz@gmail.com>
2015-05-14 21:24:23 +0200

    W miare ustawiono pierwsza czesc apki

--- app/models/nerd.js
+++ app/models/nerd.js
@@ -6,4 +6,4 @@ var mongoose = require('mongoose');
 // module.exports allows us to pass this to other files when it is called
 module.exports = mongoose.model('Nerd', {
     name : {type : String, default: ''}
-})
+});
---
+++ public/js/app.js
@@ -0,0 +1,2 @@
+// public/js/app.js
+angular.module('stock-predictor-gui', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService']);
