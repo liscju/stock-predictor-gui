16375f39a214011b008532fad8879bb20d8efcae
liscju <piotr.listkiewicz@gmail.com>
2015-05-15 12:59:47 +0200

    Dodano ekran konczacy inicjalizcje

--- public/js/controllers/WelcomeFinishCtrl.js
+++ public/js/controllers/WelcomeFinishCtrl.js
@@ -1,4 +1,7 @@
 // public/js/controllers/WelcomeFinishCtrl.js
 angular.module('WelcomeFinishCtrl', []).controller('WelcomeFinishController', function($scope,$location) {
 
+    $scope.finish = function() {
+        console.log('Kliknal finish');
+    }
 });
