821a443970fc867f6f4d8376fa85dd6a0d9ee35d
liscju <piotr.listkiewicz@gmail.com>
2015-05-15 10:59:41 +0200

    Teraz elementy sa pobierane z kontrolera

--- public/js/controllers/WelcomeStockCtrl.js
+++ public/js/controllers/WelcomeStockCtrl.js
@@ -1,4 +1,11 @@
 // public/js/controllers/WelcomeStockCtrl.js
 angular.module('WelcomeStockCtrl', []).controller('WelcomeStockController', function($scope,$location) {
     $scope.stock_prompt = "Wybierz interesujace cię spółki";
+
+    $scope.stock_list = [
+        "Cras justo odio",
+        "Dapibus ac facilisis in",
+        "Morbi leo risus",
+        "Moje elementy"
+    ];
 });
