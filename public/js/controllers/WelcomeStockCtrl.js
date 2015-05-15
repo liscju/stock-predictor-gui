e7424afac151c6a921063a47b3bdd933e8e905b9
liscju <piotr.listkiewicz@gmail.com>
2015-05-15 10:24:47 +0200

    Dalsze prace nad widokiem wyboru spolek

--- public/js/controllers/WelcomeStockCtrl.js
+++ public/js/controllers/WelcomeStockCtrl.js
@@ -1,3 +1,4 @@
 // public/js/controllers/WelcomeStockCtrl.js
 angular.module('WelcomeStockCtrl', []).controller('WelcomeStockController', function($scope,$location) {
+    $scope.stock_prompt = "Wybierz interesujace cię spółki";
 });
