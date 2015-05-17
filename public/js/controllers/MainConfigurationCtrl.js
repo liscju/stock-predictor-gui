fc9371c1878e12cffcd296d91a39571bc7599f9e
liscju <piotr.listkiewicz@gmail.com>
2015-05-17 18:48:52 +0200

    Dodano dodatkowy kontroler do obslugi konfiguracji

---
+++ public/js/controllers/MainConfigurationCtrl.js
@@ -0,0 +1,4 @@
+// public/js/controllers/MainConfigurationCtrl.js
+angular.module('MainConfigurationCtrl', []).controller('MainConfigurationController', ["$scope","$location",function($scope,$location) {
+    $scope.hello = "UDALO SIE ZALADOWAC MainConfigurationCtrl.js";
+}]);
