5635693a4705f3a4a4d9096e703d6c23a925df43
liscju <piotr.listkiewicz@gmail.com>
2015-05-17 18:59:36 +0200

    Odczytywanie wartosci do konfiguracji z serwera db

--- public/js/controllers/MainConfigurationCtrl.js
+++ public/js/controllers/MainConfigurationCtrl.js
@@ -1,4 +1,31 @@
 // public/js/controllers/MainConfigurationCtrl.js
-angular.module('MainConfigurationCtrl', []).controller('MainConfigurationController', ["$scope","$location",function($scope,$location) {
+angular.module('MainConfigurationCtrl', []).controller('MainConfigurationController', ["$scope","$location","StockService",function($scope,$location,StockService) {
     $scope.hello = "UDALO SIE ZALADOWAC MainConfigurationCtrl.js";
+
+    $scope.stock_list = [
+    ];
+
+    $scope.init = function() {
+        StockService.getStocks(
+            function (data) {
+                console.log("Success");
+                $scope.stock_list = data;
+            },function (data) {
+                console.log("Failure");
+                console.log(data);
+            });
+    };
+
+    $scope.addStock = function (stock) {
+        $scope.stock_list.push(stock);
+        $scope.stock_to_add = "";
+    };
+
+    $scope.delStock = function(stock) {
+        var indexOfStockToRemove = $scope.stock_list.indexOf(stock);
+        if (indexOfStockToRemove > -1) {
+            $scope.stock_list.splice(indexOfStockToRemove,1);
+        }
+    };
+
 }]);
